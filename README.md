84474f00ff937e3b76d9f57f04afa2639bfc5df1
goxdeveloper <mastergox.goxco@gmail.com>
2022-06-01 02:27:26 +0100

    finish first version

--- README.md
+++ README.md
@@ -1,3 +1,61 @@
 # Spotify to YouTube Music
 
-### This package is still in development...
+Convert songs from **Spotify** to **YouTube Music**!
+
+## Installation
+
+To use this package, you also need to install **[spotify-web-api-node](https://www.npmjs.com/package/spotify-web-api-node)**.
+
+```bash
+npm install spotify-to-ytmusic spotify-web-api-node
+```
+
+## Usage
+
+Before using **Spotify to YouTube Music**, you need to setup a **Spotify API client**.
+
+```javascript
+const SpotifyToYoutubeMusic = require('spotify-to-ytmusic')
+const SpotifyWebApi = require('spotify-web-api-node')
+
+// Use your Client ID, Client Secret & Redirect URI
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: 'CLIENT_ID',
+    clientSecret: 'CLIENT_SECRET',
+    redirectUri: 'REDIRECT_URI'
+})
+
+// Or use an Access Token
+
+const spotifyApi = new SpotifyWebApi()
+spotifyApi.setAccessToken('ACCESS_TOKEN')
+```
+
+Now using the **spotifyApi**, we can convert songs from **Spotify** to **YouTube Music**.
+
+```javascript
+// Setup spotify-to-ytmusic
+
+let spotifyToYoutubeMusic = SpotifyToYoutubeMusic(spotifyApi)
+
+// Convert Spotify song to YouTube Music
+
+async function convert() {
+    let song = await spotifyToYoutubeMusic('4cOdK2wGLETKBW3PvgPWqT?')
+    console.log(song) // https://www.youtube.com/watch?v=lYBUbBu4W08
+}
+convert()
+```
+
+Other ways to provide a **Spotify Track ID**.
+
+```javascript
+await spotifyToYoutubeMusic('4cOdK2wGLETKBW3PvgPWqT?')
+
+await spotifyToYoutubeMusic('https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT?')
+
+await spotifyToYoutubeMusic('spotify:track:4cOdK2wGLETKBW3PvgPWqT?')
+
+await spotifyToYoutubeMusic('https://api.spotify.com/v1/tracks/4cOdK2wGLETKBW3PvgPWqT?')
+```
--- index.js
+++ index.js
@@ -23,26 +23,47 @@ function SpotifyToYoutubeMusic(spotifyAPI) {
         }
 
         //CHECK IF URL IS ARRAY
+        
+        let isArray = true
 
         if (!Array.isArray(spotifyURL)) {
             spotifyURL = [spotifyURL]
+            isArray = false
         }
 
         //GET TRACKS
 
-        const IDs = spotifyURL.map(URL => URL.replace('spotify:track:','').replace('https://open.spotify.com/track/','').replace('https://api.spotify.com/v1/tracks/',''))
+        const IDs = spotifyURL.map(URL => URL
+            .replace('spotify:track:','')
+            .replace('https://open.spotify.com/track/','')
+            .replace('https://api.spotify.com/v1/tracks/',''))
+        .map(track => track.includes('?si=') ? track.split('?si=')[0] : track)
+
         const { tracks } = (await spotifyAPI.getTracks(IDs)).body
 
         //GET SONG(S)
 
-        let ytList = tracks.map(async (track) => await youtubeURL(track))
+        let ytList = []
 
-        async function youtubeURL() {
-            return 'youtube test :)'
+        for (let i = 0; i < tracks.length; i++) {
+
+            //SEARCH ON YOUTUBE MUSIC
+
+            let track = tracks[i]
+            let content = (await ytMusic.search(`${track.artists.map(artist => artist.name).join(' ')} ${track.name}`)).content
+            if (content.length < 1) content = (await ytMusic.search(`${track.artists[0].name} ${track.name}`)).content
+            if (content.length < 1) content = (await ytMusic.search(`${track.name} ${track.artists.map(artist => artist.name).join(' ')}`)).content
+            if (content.length < 1) content = (await ytMusic.search(`${track.name} ${track.artists[0].name}`)).content
+
+            //SELECT SONG
+
+            content = content.filter(result => result.type === 'song')
+            content.length < 1 ? ytList.push(null) : ytList.push(`https://www.youtube.com/watch?v=${content[0].videoId}`)
         }
 
         //RETURN
 
+        ytList = isArray ? ytList : ytList[0]
         return ytList
     }
 }
