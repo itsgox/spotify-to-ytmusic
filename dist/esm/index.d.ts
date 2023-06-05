81936ae20e9f1deb3e711ad17af4d30cf3204747
Noel Dufour <Noel@Noels-MacBook-Pro.local>
2023-06-05 17:38:04 +0200

    Deleting dist directory

--- dist/esm/index.d.ts
+++
@@ -1,6 +0,0 @@
-export default function SpotifyToYouTubeMusic({ clientID, clientSecret, accessToken, ytMusicUrl, }: {
-    clientID: string;
-    clientSecret: string;
-    accessToken: string;
-    ytMusicUrl: string;
-}): Promise<(spotifyID: string | string[]) => Promise<string[] | null>>;
